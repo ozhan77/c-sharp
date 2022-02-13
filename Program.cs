2006fdfdbfa487aa9bdd196cc32e3bb48bd6f8f3
ogz <oguzhandky@gmail.com>
2022-02-13 23:31:53 +0300

    variables

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string str1= string.Empty;
+str1 = "oguzhan daskaya";
+string ad = "oguz";
+string soyad= "daskaya";
+string tamisim= ad+ soyad;
+//
+int integer1= 5;
+int integer2 =3;
+int integer53 = integer1+integer2;
+//
+bool bool1=10>2;
+string str="44";
+int int12=51;
+int newValue=integer53*Convert.ToInt32(str);
+Console.WriteLine(newValue);
+
+int newValue2=integer2*Convert.ToInt32(str);
+Console.WriteLine(newValue2);
+
+string datenow= DateTime.Now.ToString();
+Console.WriteLine(datenow);
+
+//Yeter bu kadar.
+
