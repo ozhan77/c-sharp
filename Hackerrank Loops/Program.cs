5f74079a00d64ca165f375ac1cdd37473adffdfa
ogz <oguzhandky@gmail.com>
2022-02-16 23:59:53 +0300

    loop example

---
+++ Hackerrank Loops/Program.cs
@@ -0,0 +1,17 @@
+﻿int n = 5;
+string txt="";
+
+for (int i = 1; i <= n; i++)
+{
+    txt="";
+    int j = n-i;
+    for (;j >= 0; j--)
+    {
+        txt += " ";
+    }
+    for (int k = 0; k < i; k++)
+    {
+        txt += "#";
+    }
+    Console.WriteLine(txt);
+}
