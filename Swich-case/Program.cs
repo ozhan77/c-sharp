8756382ca2acd95cb786467d011d6800f296f5c3
ogz <oguzhandky@gmail.com>
2022-02-16 16:50:58 +0300

    kafalar karıştı

---
+++ Swich-case/Program.cs
@@ -0,0 +1,27 @@
+﻿int month = DateTime.Now.Month;
+
+
+
+switch (month)
+{
+    case 1:
+        Console.WriteLine("Ocak");break;
+    case 2:
+        Console.WriteLine("Kapıdan baktık");break;
+    case 3:
+        Console.WriteLine("İlk bahar geliyor");break;
+    case 4:
+        Console.WriteLine("bilgisayar bozuldu");break;
+    case 5:
+        Console.WriteLine("vürüs bitse kurtulsak");break;
+    case 6:
+    case 7:
+    case 8:
+        Console.WriteLine("Yaz geldi deniz gezme müzük aşk");break;
+    case 9:
+    case 10:
+    case 11:
+        Console.WriteLine("depresyon");break;
+
+    
+}
