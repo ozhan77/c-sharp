9715866a43be14f54fc2b4a0ec8af27598af5b57
ogz <oguzhandky@gmail.com>
2022-02-18 00:00:55 +0300

    foreach

---
+++ Hackerrank while/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] arr = {-2,3,4,0,-1};
+decimal pozitif=0,negatif=0,zero=0;
+foreach (var item in arr)
+{
+    if (item>0)
+    {
+        pozitif++;
+    }
+    else if (item<0)
+    {
+        negatif++;
+    }
+    else
+    {
+        zero++;
+    }
+    
+}
+Console.WriteLine(pozitif/arr.Length);
+Console.WriteLine(negatif/arr.Length);
+Console.WriteLine(zero/arr.Length);
+
