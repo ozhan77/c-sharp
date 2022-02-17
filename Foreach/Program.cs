b8676caafe7cbd8396bc29483c862fac1b6f4ff1
ogz <oguzhandky@gmail.com>
2022-02-18 00:10:25 +0300

    bunlar iyzi

---
+++ Foreach/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] beden={"s","m","L","XL"};
+string[] filmler={"ishiyarlara yer yok","das boot","the god fadır","kurtlar vadisi ırak"};
+Console.WriteLine(filmler[3]);
+foreach (var item in filmler)
+{
+    Console.WriteLine(item);
+}
