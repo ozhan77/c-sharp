dcdc7a019358f2f3d23207304f1c28a7ad38f8ca
ogz <oguzhandky@gmail.com>
2022-02-20 00:02:09 +0300

    array

---
+++ Array/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] arr = {1,2,5,7,8,4,7,5};
+
+foreach (var item in arr)
+{
+    Console.WriteLine(item);
+}
+Array.Reverse(arr);
+
+foreach (var item in arr)
+{
+    Console.WriteLine(item);
+}
+
+Array.Clear(arr,1,3);
+
+foreach (var item in arr)
+{
+    Console.WriteLine(item);
+}
+Array.Reverse(arr);
+
+foreach (var item in arr)
+{
+    Console.WriteLine(item);
+}
