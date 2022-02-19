0435ddf1a5be9133a7228f637deb1f7fd5ecfdaa
ogz <oguzhandky@gmail.com>
2022-02-19 23:34:12 +0300

    array

---
+++ hacker-rank for/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] candles={5,3,4,2,2,4,5,3,5,5};
+int high=0,count=0;
+for (int i = 1; i < candles.Count(); i++)
+{
+    if (candles[i]>high)
+    {
+        high=candles[i];
+    }
+}
+var total = candles.Count(n=> n==high);
+Console.WriteLine(total);
