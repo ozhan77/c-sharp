6c75b2f989628fb649da5f1a281ac8c452584f7b
ogz <oguzhandky@gmail.com>
2022-02-20 23:49:24 +0300

    referans

---
+++ method/Program.cs
@@ -0,0 +1,30 @@
+﻿int number_1 = 124,a=231;
+metotlar mt = new metotlar();
+Console.WriteLine(number_1);
+
+static int topla(int x, int y)
+{
+    return x+y;
+}
+
+Console.WriteLine(topla(number_1,a));
+
+mt.println("dfsdgafgv");
+
+
+mt.println((mt.raise(ref number_1,ref a).ToString()));
+
+
+class metotlar
+{
+    public void println(string txt)
+    {
+        Console.WriteLine(txt);
+    }
+
+    public int raise(ref int a, ref int b)
+    {
+        a++;b++;
+        return a+b;
+    }
+}
