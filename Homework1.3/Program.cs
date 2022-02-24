6f55f319a6d5220d2429093389f3773d02357ae7
ogz <oguzhandky@gmail.com>
2022-02-25 00:47:30 +0300

    1.3

---
+++ Homework1.3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("enter the mayrrix");
+int adet=Convert.ToInt32(Console.ReadLine());
+string[] arr= new string[adet];
+
+Console.WriteLine(adet + " adet kelime gir bakim");
+
+for (int i = 0; i < adet; i++)
+{
+    arr[i]=Console.ReadLine();
+}
+foreach (var item in arr.Reverse())
+{
+    Console.WriteLine(item);
+}
