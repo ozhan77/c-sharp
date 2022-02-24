46a19cae9ac3b83269d97b1dc9880c91a6ab55f5
ogz <oguzhandky@gmail.com>
2022-02-25 00:58:57 +0300

    1.4

---
+++ Homework 1.4/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("cümle yaz ulan kel");
+string txt=Console.ReadLine();
+string[] dilim=txt.Split(' ');
+Console.WriteLine ("Harf sayısı:"+ Convert.ToString(txt.Length-dilim.Length+1) );
+Console.WriteLine ("Kelime sayısı:"+Convert.ToString(dilim.Length));
