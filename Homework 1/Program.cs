b9989920828b328bce54ec4b690cf99e8cf74c8d
ogz <oguzhandky@gmail.com>
2022-02-25 00:23:41 +0300

    1.1

---
+++ Homework 1/Program.cs
@@ -0,0 +1,23 @@
+﻿int number1;
+
+Console.WriteLine("Bir sayı girin");
+number1=Convert.ToInt16(Console.ReadLine());
+
+int[] arr = new int[number1];
+Console.WriteLine(number1+" Adet sayı girin");
+
+for (int i = 0; i < number1; i++)
+{
+    
+    arr[i]= Convert.ToInt16(Console.ReadLine());
+    
+}
+Console.WriteLine("Çift Sayılar");
+foreach (var item in arr)
+{
+    if (0==item % 2)
+    {
+        Console.WriteLine(item);
+    }
+}
+
