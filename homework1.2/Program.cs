0e3cb2940da93d47dfab5392f9a50dd03eaf8416
ogz <oguzhandky@gmail.com>
2022-02-25 00:37:03 +0300

    1.2

---
+++ homework1.2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("2 sayı gir");
+
+int x=Convert.ToInt32(Console.ReadLine()),y=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(x+" adet sayı gir la!");
+int[] arr = new int[x];
+for (int i = 0; i < x; i++)
+{
+    arr[i]=Convert.ToInt32(Console.ReadLine());
+}
+foreach (var item in arr)
+{
+    if (item==y || 0 == item % y)
+    {
+        Console.WriteLine(item);
+    }
+}
