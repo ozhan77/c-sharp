6eb804672e5222da8d16127ab65853553364ac73
ogz <oguzhandky@gmail.com>
2022-02-26 01:29:11 +0300

    array

---
+++ Arraylist/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+ArrayList arr = new ArrayList();
+
+arr.Add("5lira");
+arr.Add("2 adım");
+arr.Add(2);
+arr.Add("bir geri");
+
+foreach (var item in arr)
+{
+    Console.WriteLine(item);
+}
+
+string[] tool={"çekiç","tornavida","silikon"};
+int[] numbers={1,4,6,7,3,3234,543,};
+List<int> numberlist= new List<int>(numbers);
+//arr.Add(tool);
+//arr.Add(numberlist);
+arr.Sort();
+
+foreach (var item in arr)
+{
+    Console.WriteLine(item);
+}
