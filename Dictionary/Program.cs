cef96b82f207b3447feda0612e0ea28904282e14
ogz <oguzhandky@gmail.com>
2022-02-26 23:43:13 +0300

    dictionary

---
+++ Dictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿Dictionary<int,string> users = new Dictionary<int, string>();
+users.Add(1,"oz");
+users.Add(2,"umut");
+users.Add(3,"bürütüs");
+
+Console.WriteLine(users[2]);
+
+foreach (var item in users)
+{
+    Console.WriteLine(item);
+}
+users.Remove(6);
+
+
+foreach (var item in users.Keys)
+{
+    Console.WriteLine(item);
+}
+foreach (var item in users.Values)
+{
+    Console.WriteLine(item);
+}
