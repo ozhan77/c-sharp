824e1e64db0253bfa0c2ae7c1daac99d525562b0
ogz <oguzhandky@gmail.com>
2022-02-26 20:04:46 +0300

    new exception

---
+++ exception/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+//Write your code here
+
+class Calculator {
+    public int power(int a,int b){
+        return a^b;
+    }
+}
+
+
+class Solution{
+    static void Main(String[] args){
+        Calculator myCalculator=new  Calculator();
+        int T=Int32.Parse(Console.ReadLine());
+        while(T-->0){
+            string[] num = Console.ReadLine().Split();
+            int n = int.Parse(num[0]);
+            int p = int.Parse(num[1]); 
+            try{
+                int ans=myCalculator.power(n,p);
+                Console.WriteLine(ans);
+            }
+            catch(Exception e){
+               Console.WriteLine(e.Message);
+
+            }
+        }
+        
+        
+        
+    }
+}
