dd9a4e0fb0f40f82386fb816a45e9b6d0eab9e9e
ogz <oguzhandky@gmail.com>
2022-03-07 01:42:29 +0300

    karpuz keselim

---
+++ Enum/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Console.WriteLine(days.sunday);
+Console.WriteLine((int)days.mondoay);
+
+if (10<(int)tempcure.warm)
+{
+    Console.WriteLine("üşüdük reiz");
+}
+else if (-10<(int)tempcure.cold)
+{
+    Console.WriteLine("soba yakalım");
+}
+else if (50>(int)tempcure.hell)
+{
+    Console.WriteLine("yanıyoz aq");
+}
+
+enum days
+{
+    mondoay,sunday,tuesday,wednesday,tursdayfriday,saturday
+}
+enum tempcure
+{
+    toCold=-10,cold =0,normal=15,warm=25,hot=35,hell=42
+}
