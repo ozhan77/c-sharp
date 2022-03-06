adbafffec13324fc23b9afe1e05ffa10cbdb8bbb
ogz <oguzhandky@gmail.com>
2022-03-06 16:34:00 +0300

    ödev3

---
+++ Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+ArrayList vowels=new ArrayList(){"a","e","i","o","u"};
+ArrayList result=new ArrayList();
+
+string[] trueVowel={"a","e","i","o","u"};
+
+Console.WriteLine("ali tut");
+string txt = Console.ReadLine();
+
+for (int i = 0; i < txt.Length; i++)
+{
+    Console.WriteLine("txt değeri"+txt[i]);
+    if (trueVowel.Contains(txt[i].ToString()))
+    {
+        result.Add(txt[i].ToString());
+    }
+    
+}
+result.Sort();
+foreach (var item in result)
+    {
+        Console.WriteLine(item);
+    }
