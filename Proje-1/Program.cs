a12e004a34bfb574ae0f54ad6cd08661dcd07df1
ogz <oguzhandky@gmail.com>
2022-03-16 17:23:14 +0300

    fundamentals menu

---
+++ Proje-1/Program.cs
@@ -0,0 +1,25 @@
+﻿menus menu = new menus();
+
+menu.mainMenu();
+
+
+
+class menus
+{
+    public void mainMenu(){
+        Console.WriteLine("Lütfen yapmak istediğiniz işlemi seçiniz :)");
+        Console.WriteLine("*******************************************");
+        Console.WriteLine("(1) Yeni Numara Kaydetmek");
+        Console.WriteLine("(2) Varolan Numarayı Silmek");
+        Console.WriteLine("(3) Varolan Numarayı Güncelleme");
+        Console.WriteLine("(5) Rehberde Arama Yapmak");
+    }
+    public void addNumber(){
+        Console.WriteLine("Lütfen isim giriniz             :");
+        Console.WriteLine("Lütfen soyisim giriniz          :");
+        Console.WriteLine("Lütfen telefon numarası giriniz :");
+    }
+    public void removeNumber(){
+        Console.WriteLine("Lütfen telefon numarası giriniz :");
+    }
+}
